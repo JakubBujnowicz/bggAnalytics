24b85d40db5b3f111692946dd2440df7dc28a10b
Jakub Bujnowicz <bujnowiczjakub@gmail.com>
2020-10-05 12:50:45 +0200

    Function detect_namestyle renamed to bgg_namestyle and moved to bgg_tools file

--- R/bgg_tools.R
+++ R/bgg_tools.R
@@ -1,3 +1,28 @@
+#' Get BGG URLs for games with given IDs
+#'
+#' This function is a simple wrapper that returns URLs to given games by using
+#' their IDs.
+#'
+#' @param ids a numeric vector of positive integers.
+#'
+#' @return A character vector of the same length as \code{x}, contains URLs.
+#'
+#' @author Jakub Bujnowicz \email{bujnowiczgithub@@gmail.com}
+#' @export
+#'
+#' @examples
+#' bgg_gameurl(1:10)
+#'
+bgg_gameurl <- function(ids)
+{
+    # Assertions
+    assert_that(.are_positive_integers(ids))
+
+    result <- paste0(.bgg_url("boardgame"), ids)
+    return (result)
+}
+
+
 #' Merge data from two bggAPI objects
 #'
 #' This allows for merging data from two bggAPI objects. Namestyle is inherited
@@ -42,6 +67,61 @@ bgg_merge <- function(x, y, ...)
     ydata <- ydata[, y_cols, with = FALSE]
 
     result <- merge(xdata, ydata, ...)
+    return(result)
+}
+
+
+#' Detect the namestyle of the data table
+#'
+#' Detects whether the \code{dt} data.table was created by a \code{bggAPI}
+#' object with \code{'pretty'} or \code{'classic'} names. Ends with the error if
+#' one of them cannot be unequivocally determined. This can be useful when
+#' working on a modified table that is no longer directly connected to any
+#' \code{bggAPI} object.
+#'
+#' @param dt a data.table from \code{data} slot of a \code{bggAPI} object.
+#'
+#' @return A single string.
+#'
+#' @author Jakub Bujnowicz \email{bujnowiczgithub@@gmail.com}
+#' @export
+#'
+#' @examples
+#' gm <- bggGames$new(ids = 167791)
+#' bgg_namestyle(gm$data)
+#'
+#' gm$switch_namestyle("pretty")
+#' bgg_namestyle(gm$data)
+#'
+#' # Breaks
+#' # bgg_namestyle(iris)
+#'
+bgg_namestyle <- function(dt)
+{
+    # Assertions
+    assert_that(is.data.frame(dt))
+
+    dt_names <- names(dt)
+    cl_names <- var_specs$Variable
+    pt_names <- var_specs$PrettyName
+
+    cl_count <- sum(dt_names %in% cl_names)
+    pt_count <- sum(dt_names %in% pt_names)
+
+    result <- fcase(cl_count > 0 &
+                        pt_count > 0,    "both",
+                    cl_count > pt_count, "classic",
+                    pt_count > cl_count, "pretty",
+                    default =            "zeros")
+
+    dt_name <- deparse(substitute(dt))
+
+    if (result == "zeros") {
+        stop("no 'pretty' nor 'classic' names found in '", dt_name, "'")
+    } else if (result == "both") {
+        stop("found both 'pretty' and 'classic' names in '", dt_name, "'")
+    }
+
     return(result)
 }
 
@@ -93,29 +173,4 @@ bgg_topgames <- function(places = 1:100)
 
     result <- result[match(unlist(pages), places)]
     return(result)
-}
-
-
-#' Get BGG URLs for games with given IDs
-#'
-#' This function is a simple wrapper that returns URLs to given games by using
-#' their IDs.
-#'
-#' @param ids a numeric vector of positive integers.
-#'
-#' @return A character vector of the same length as \code{x}, contains URLs.
-#'
-#' @author Jakub Bujnowicz \email{bujnowiczgithub@@gmail.com}
-#' @export
-#'
-#' @examples
-#' bgg_gameurl(1:10)
-#'
-bgg_gameurl <- function(ids)
-{
-    # Assertions
-    assert_that(.are_positive_integers(ids))
-
-    result <- paste0(.bgg_url("boardgame"), ids)
-    return (result)
 }
